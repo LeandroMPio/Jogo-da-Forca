0291bd4f46996525a23c76a455ff2a1d2f9a596d
LeandroMPio <leandropio@gmail.com>
2023-09-19 17:35:17 -0300

    add: tema e quantidade de letras palavra-chave

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace JogoDaForca
 {
@@ -6,27 +7,44 @@ namespace JogoDaForca
     {
         static void Main(string[] args)
         {
-            string palavraChave = "";
-            Console.WriteLine("Digite a palavra-chave:");
-            palavraChave = Console.ReadLine();
-            Console.Clear();
-
-            string[] letrasCorretas = new string[palavraChave.Length];
             int numeroTentativas = 0;
             int quantidadeAcertos = 0;
             int numeroMaximoTentativas = 6;
 
-            
+            string palavraChave = "";
+            Console.WriteLine($"Vamos Jogar Forca, você pode errar no máximo {numeroMaximoTentativas} vezes.");
+            Console.WriteLine("Digite o tema da palavra-chave");
+            string tema = Console.ReadLine();
+            Console.WriteLine("Digite a palavra-chave para começar o jogo:");
+            palavraChave = Console.ReadLine().ToLower();
+            Console.Clear();
+
+            string[] letrasCorretas = new string[palavraChave.Length];
 
             do
-            {                
+            {
+                Console.WriteLine("----------------------------------------");
+                Console.WriteLine($"O tema da palavra-chave é {tema}");
+                Console.WriteLine($"A palavra-chave contém {palavraChave.Length} letras");
+                Console.WriteLine("----------------------------------------");
+                Console.WriteLine("");
                 Console.WriteLine("informe uma letra");
                 string letra = Console.ReadLine().ToLower();
+
+                if(!Regex.IsMatch(letra, @"^[a-zA-Z]+$"))
+                {
+                    Console.WriteLine("Entrada Inválida");
+                    continue;
+                }
 
                 if (palavraChave.ToLower().Contains(letra))
                 {
                     for (int i = 0; i < palavraChave.Length; i++)
                     {
+                        if (letrasCorretas[i] == "")
+                        {
+                            letrasCorretas[i] = "_";
+                        }
                         if (palavraChave[i].ToString() == letra)
                         {
                             letrasCorretas[i] = letra;
@@ -50,49 +68,51 @@ namespace JogoDaForca
                 switch (numeroTentativas)
                 {
                     case 1:
-                        Console.WriteLine("|----");
+                        Console.WriteLine("|ˉˉˉˉ|");
                         Console.WriteLine("|    O");
                         Console.WriteLine("|");
                         Console.WriteLine("|");
                         Console.WriteLine("|");
                         break;
                     case 2:
-                        Console.WriteLine("|----");
+                        Console.WriteLine("|ˉˉˉˉ|");
                         Console.WriteLine("|    O");
                         Console.WriteLine("|    |");
                         Console.WriteLine("|");
                         Console.WriteLine("|");
                         break;
                     case 3:
-                        Console.WriteLine("|----");
+                        Console.WriteLine("|ˉˉˉˉ|");
                         Console.WriteLine("|    O");
                         Console.WriteLine("|   /|");
                         Console.WriteLine("|    ");
                         Console.WriteLine("|");
                         break;
                     case 4:
-                        Console.WriteLine("|----");
+                        Console.WriteLine("|ˉˉˉˉ|");
                         Console.WriteLine("|    O");
                         Console.WriteLine("|   /|\\");
                         Console.WriteLine("|");
                         Console.WriteLine("|");
                         break;
                     case 5:
-                        Console.WriteLine("|----");
+                        Console.WriteLine("|ˉˉˉˉ|");
                         Console.WriteLine("|    O");
                         Console.WriteLine("|   /|\\");
                         Console.WriteLine("|    |");
                         Console.WriteLine("|   / ");
                         break;
                     case 6:
-                        Console.WriteLine("|----");
+                        Console.WriteLine("|ˉˉˉˉ|");
                         Console.WriteLine("|    O");
                         Console.WriteLine("|   /|\\");
                         Console.WriteLine("|    |");
                         Console.WriteLine("|   / \\");
+                        Console.WriteLine("");
+                        Console.WriteLine("Você foi enforcado, boa sorte na proxima vez");
                         break;
                     default:
-                        Console.WriteLine("|----");
+                        Console.WriteLine("|ˉˉˉˉ|");
                         Console.WriteLine("|    ");
                         Console.WriteLine("|");
                         Console.WriteLine("|");
